67f0877c28bb5e067d750cca9b9644088365cc6a
Top-Men <yuanyang330445@gmail.com>
2026-08-19 07:37:39 +0800

    Console: install loopback Host request guard

--- src/tonmen/dashboard/__init__.py
+++ src/tonmen/dashboard/__init__.py
@@ -1,3 +1,12 @@
-from .server import DashboardState, mission_payload, serve_dashboard, validate_console_host
+from .server import DashboardState, TonmenDashboardHandler, mission_payload, serve_dashboard, validate_console_host
+from .request_guard import install_loopback_request_guard, validate_loopback_host_header
 
-__all__ = ["DashboardState", "mission_payload", "serve_dashboard", "validate_console_host"]
+install_loopback_request_guard(TonmenDashboardHandler)
+
+__all__ = [
+    "DashboardState",
+    "mission_payload",
+    "serve_dashboard",
+    "validate_console_host",
+    "validate_loopback_host_header",
+]
