49373fc45a32ff7352c5f90abba543c8ef0aeb67
Top-Men <yuanyang330445@gmail.com>
2026-08-18 16:26:16 +0300

    AI: export capability preference type

--- src/tonmen/ai/__init__.py
+++ src/tonmen/ai/__init__.py
@@ -1,8 +1,9 @@
-from .model import AIAdvisory, AIHypothesis, AIProviderError, AIProviderStatus
+from .model import AICapabilityPreference, AIAdvisory, AIHypothesis, AIProviderError, AIProviderStatus
 from .ollama import OllamaProvider
 from .service import LocalAIService
 
 __all__ = [
+    "AICapabilityPreference",
     "AIAdvisory",
     "AIHypothesis",
     "AIProviderError",
