b4bae98643107649ea947b468b89b3a17234194e
Top-Men <yuanyang330445@gmail.com>
2026-08-18 10:11:57 +0300

    Models: add optional local model runtime package

---
+++ src/tonmen/models/__init__.py
@@ -0,0 +1,10 @@
+from .config import ModelRuntimeConfig
+from .runtime import ModelAgentReview, ModelRuntime, ModelRuntimeError, ModelRuntimeStatus
+
+__all__ = [
+    "ModelAgentReview",
+    "ModelRuntime",
+    "ModelRuntimeConfig",
+    "ModelRuntimeError",
+    "ModelRuntimeStatus",
+]
