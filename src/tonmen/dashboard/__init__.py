b5ba6080a4b32cda5a23a023b31fac03415a667e
Top-Men <yuanyang330445@gmail.com>
2026-08-19 07:40:48 +0800

    Console: require verified Audit reads

--- src/tonmen/dashboard/__init__.py
+++ src/tonmen/dashboard/__init__.py
@@ -1,7 +1,9 @@
 from .server import DashboardState, TonmenDashboardHandler, mission_payload, serve_dashboard, validate_console_host
 from .request_guard import install_loopback_request_guard, validate_loopback_host_header
+from .state_guard import install_verified_audit_reader
 
 install_loopback_request_guard(TonmenDashboardHandler)
+install_verified_audit_reader(DashboardState)
 
 __all__ = [
     "DashboardState",
