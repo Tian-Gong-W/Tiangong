8ef73883d156289fcea0f6502457ccd23744591e
Top-Men <yuanyang330445@gmail.com>
2026-08-20 01:58:02 +0800

    Console: route dashboard entry through Provider Hub server

--- src/tonmen/dashboard/__init__.py
+++ src/tonmen/dashboard/__init__.py
@@ -1,3 +1,4 @@
-from .server import DashboardState, mission_payload, serve_dashboard, validate_console_host
+from .provider_server import DashboardState, serve_dashboard
+from .server import mission_payload, validate_console_host
 
 __all__ = ["DashboardState", "mission_payload", "serve_dashboard", "validate_console_host"]
