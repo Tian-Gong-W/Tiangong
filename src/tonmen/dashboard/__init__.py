abcee0fd11c1a8d18e087fe8bafc17e32171cedb
Top-Men <yuanyang330445@gmail.com>
2026-08-21 03:31:00 +0800

    console: route dashboard through mission preflight layer

--- src/tonmen/dashboard/__init__.py
+++ src/tonmen/dashboard/__init__.py
@@ -1,4 +1,4 @@
-from .provider_server import DashboardState, serve_dashboard
+from .preflight_server import DashboardState, serve_dashboard
 from .server import mission_payload, validate_console_host
 
 __all__ = ["DashboardState", "mission_payload", "serve_dashboard", "validate_console_host"]
