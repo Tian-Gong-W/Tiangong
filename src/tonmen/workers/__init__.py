fa53e01c4d160285f911c800faaadd0256ddef7e
Top-Men <yuanyang330445@gmail.com>
2026-08-20 18:43:47 +0800

    Worker: export scheduler types

--- src/tonmen/workers/__init__.py
+++ src/tonmen/workers/__init__.py
@@ -1,6 +1,7 @@
 from .executor import RemoteWorkerExecutor
 from .pool import WorkerPool, WorkerSpec, WorkerState
 from .protocol import DispatchEnvelope, normalize_worker_id, require_worker_secret
+from .scheduler import WorkerLease, WorkerQueueFull, WorkerQueueTimeout, WorkerScheduler
 from .server import WorkerService, serve_worker, validate_worker_bind_host
 from .transport import WorkerHTTPTransport, WorkerTransportError
 
@@ -8,7 +9,11 @@ __all__ = [
     "DispatchEnvelope",
     "RemoteWorkerExecutor",
     "WorkerHTTPTransport",
+    "WorkerLease",
     "WorkerPool",
+    "WorkerQueueFull",
+    "WorkerQueueTimeout",
+    "WorkerScheduler",
     "WorkerService",
     "WorkerSpec",
     "WorkerState",
