848dedc8a57a099a2e20dbd3db9c76f2a4fd9870
Top-Men <yuanyang330445@gmail.com>
2026-08-21 17:36:28 +0800

    console: route through simplified module view

--- src/tonmen/dashboard/__init__.py
+++ src/tonmen/dashboard/__init__.py
@@ -1,4 +1,4 @@
-from .usability_server import DashboardState, serve_dashboard
+from .simple_view_server import DashboardState, serve_dashboard
 from .server import mission_payload, validate_console_host
 
 __all__ = ["DashboardState", "mission_payload", "serve_dashboard", "validate_console_host"]
