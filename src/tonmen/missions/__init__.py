493cf1e2ee459647ebdfd5b5980cf20ca81135c6
Top-Men <yuanyang330445@gmail.com>
2026-08-23 09:09:55 +0800

    Export structured ActionOutcome model

--- src/tonmen/missions/__init__.py
+++ src/tonmen/missions/__init__.py
@@ -1,9 +1,17 @@
 from .ledger import ActionLedger
 from .model import MissionPlan, MissionStep, StepState
+from .outcome import (
+    ActionOutcome,
+    ActionOutcomeKind,
+    classify_proposal_outcome,
+    record_action_outcome,
+)
 from .run import MissionRun, MissionRunState, StepExecution, StepExecutionState, iter_plan_executions
 
 __all__ = [
     "ActionLedger",
+    "ActionOutcome",
+    "ActionOutcomeKind",
     "MissionPlan",
     "MissionRun",
     "MissionRunState",
@@ -11,5 +19,7 @@ __all__ = [
     "StepExecution",
     "StepExecutionState",
     "StepState",
+    "classify_proposal_outcome",
     "iter_plan_executions",
+    "record_action_outcome",
 ]
