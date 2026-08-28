f8bbd8724cb6f7a06c787bc54f4d4f0372035549
Top-Men <yuanyang330445@gmail.com>
2026-08-29 04:26:34 +0800

    Register expanded discovery adapters

--- src/tonmen/tools/adapters/__init__.py
+++ src/tonmen/tools/adapters/__init__.py
@@ -1,12 +1,23 @@
 from .httpx import HttpxAdapter
+from .katana import KatanaAdapter
 from .nmap import NmapAdapter
 from .nuclei import NucleiAdapter
+from .subfinder import SubfinderAdapter
 
 
 def register_builtin_adapters(registry) -> None:
     registry.register(NmapAdapter())
+    registry.register(SubfinderAdapter())
     registry.register(HttpxAdapter())
+    registry.register(KatanaAdapter())
     registry.register(NucleiAdapter())
 
 
-__all__ = ["HttpxAdapter", "NmapAdapter", "NucleiAdapter", "register_builtin_adapters"]
+__all__ = [
+    "HttpxAdapter",
+    "KatanaAdapter",
+    "NmapAdapter",
+    "NucleiAdapter",
+    "SubfinderAdapter",
+    "register_builtin_adapters",
+]
